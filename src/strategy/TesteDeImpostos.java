adf2b41b381565389ade83a296d89f7614ced217
ijkmoises <ijkmoises@gmail.com>
2020-09-11 16:07:52 -0300

    Adicionado imposto ICCC

--- src/strategy/TesteDeImpostos.java
+++ src/strategy/TesteDeImpostos.java
@@ -5,6 +5,7 @@ public class TesteDeImpostos {
 	public static void main(String[] args) {
 		Imposto iss = new ISS();
 		Imposto icms = new ICMS();
+		Imposto novoImposto = new ICCC();
 		
 		Orcamento orcamento = new Orcamento(500.0);
 		
@@ -12,5 +13,7 @@ public class TesteDeImpostos {
 		
 		calculadora.realizaCalculo(orcamento, iss);
 		calculadora.realizaCalculo(orcamento, icms);
+		calculadora.realizaCalculo(orcamento, novoImposto);
+
 	}
 }
