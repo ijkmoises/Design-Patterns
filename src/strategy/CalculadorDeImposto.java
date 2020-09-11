1cbb334fced05336ea029969246bc746b4ed3b59
ijkmoises <ijkmoises@gmail.com>
2020-09-11 15:43:59 -0300

    Implementado padrão Strategy

---
+++ src/strategy/CalculadorDeImposto.java
@@ -0,0 +1,9 @@
+package strategy;
+
+public class CalculadorDeImposto {
+	
+	public void realizaCalculo(Orcamento orcamento, Imposto impostoQualquer) {
+		double retorno = impostoQualquer.calcula(orcamento);
+		System.out.println(retorno);
+	}
+}
---
+++ src/strategy/TesteDeImpostos.java
@@ -0,0 +1,16 @@
+package strategy;
+
+public class TesteDeImpostos {
+	
+	public static void main(String[] args) {
+		Imposto iss = new ISS();
+		Imposto icms = new ICMS();
+		
+		Orcamento orcamento = new Orcamento(500.0);
+		
+		CalculadorDeImposto calculadora = new CalculadorDeImposto();
+		
+		calculadora.realizaCalculo(orcamento, iss);
+		calculadora.realizaCalculo(orcamento, icms);
+	}
+}
