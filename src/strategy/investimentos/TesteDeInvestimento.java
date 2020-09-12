b8d4665fe3317f0d2fe86bc92299e266f8690fd4
ijkmoises <ijkmoises@gmail.com>
2020-09-12 13:22:07 -0300

    Finalizada implementacao do padrao Strategy

--- src/strategy/investimentos/TesteDeInvestimento.java
+++ src/strategy/investimentos/TesteDeInvestimento.java
@@ -2,6 +2,14 @@ package strategy.investimentos;
 
 public class TesteDeInvestimento {
 	public static void main(String[] args) {
+		RealizadorDeInvestimentos realizador = new RealizadorDeInvestimentos();
 		
+		realizador.realiza(new Conta(300), new Conservador());
+		
+		
+		realizador.realiza(new Conta(300), new Moderado());
+		
+		
+		realizador.realiza(new Conta(300), new Arrojado());
 	}
 }
